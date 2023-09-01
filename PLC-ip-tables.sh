e2fd72a84b47f4dec03156d23e4b4892a5dd049f
gs4162 <81384757+gs4162@users.noreply.github.com>
2023-09-01 14:23:59 +1200

    Update PLC-ip-tables.sh

--- PLC-ip-tables.sh
+++ PLC-ip-tables.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Debug: Show current directory and files
+echo "Debug: Current directory and files"
+ls -al
+
 # Check if iptables-persistent is installed and install it if not
 dpkg -l | grep -qw iptables-persistent || sudo apt-get update && sudo apt-get install -y iptables-persistent
 
@@ -9,6 +13,9 @@ read -p "Please enter the PLC IP address: " plc_ip
 # Extract the network part of the PLC IP address for Tailscale
 IFS='.' read -ra ADDR <<< "$plc_ip"
 plc_network="${ADDR[0]}.${ADDR[1]}.${ADDR[2]}.0/24"
+
+# Debug: Print plc_network
+echo "Debug: plc_network is set to $plc_network"
 
 # Extract the Tailscale IP address from the system
 tailscale_ip=$(ip addr show tailscale0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
@@ -36,7 +43,10 @@ echo "iptables configured and saved successfully."
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
 sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+
+# Debug: Run Tailscale command and print its return code
 sudo tailscale up --advertise-routes=$plc_network
+echo "Tailscale command returned $?"
 
 # Remove this script
 rm -- "$0"
