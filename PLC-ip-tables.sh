a434489eb74662abceaa97b9b6fd49cb42aaf6b2
gs4162 <81384757+gs4162@users.noreply.github.com>
2023-09-04 14:07:29 +1200

    Update PLC-ip-tables.sh

--- PLC-ip-tables.sh
+++ PLC-ip-tables.sh
@@ -4,16 +4,15 @@
 echo "Debug: Current directory and files"
 ls -al
 
-# Preconfigure and install iptables-persistent if not already installed
-echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
-echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+# Install iptables-persistent if not installed
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y iptables-persistent
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y iptables-persistent || exit 1
 
 # Ask user for the PLC IP address
 read -p "Please enter the PLC IP address: " plc_ip
+echo "Debug: PLC IP = $plc_ip"
 
-# Remove previous rules (now placed after asking for plc_ip)
+# Remove previous rules
 sudo iptables -t nat -D PREROUTING -p tcp --dport 44818 -j DNAT --to-destination $plc_ip:44818 2>/dev/null
 sudo iptables -t nat -D PREROUTING -p udp --dport 44818 -j DNAT --to-destination $plc_ip:44818 2>/dev/null
 sudo iptables -D FORWARD -p tcp -d $plc_ip --dport 44818 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT 2>/dev/null
@@ -31,33 +30,23 @@ sudo iptables -P FORWARD ACCEPT
 sudo iptables -P OUTPUT ACCEPT
 
 # Save iptables rules
-sudo netfilter-persistent save
+sudo netfilter-persistent save || exit 1
 
-# Extract the Tailscale IP address from the system
+# Extract the Tailscale IP address
 tailscale_ip=$(ip addr show tailscale0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+echo "Debug: Tailscale IP = $tailscale_ip"
 
-# Check if Tailscale IP was successfully extracted
+# Check if Tailscale IP was extracted
 if [[ -z "$tailscale_ip" ]]; then
   echo "Error: Could not find Tailscale IP."
   exit 1
 fi
 
 # Add new iptables rules
-sudo iptables -t nat -A PREROUTING -p tcp -d $tailscale_ip --dport 44818 -j DNAT --to-destination $plc_ip:44818
-sudo iptables -t nat -A PREROUTING -p udp -d $tailscale_ip --dport 44818 -j DNAT --to-destination $plc_ip:44818
-sudo iptables -A FORWARD -p tcp -d $plc_ip --dport 44818 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-sudo iptables -A FORWARD -p udp -d $plc_ip --dport 44818 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -t nat -A PREROUTING -p tcp -d $tailscale_ip --dport 44818 -j DNAT --to-destination $plc_ip:44818 || exit 1
+sudo iptables -t nat -A PREROUTING -p udp -d $tailscale_ip --dport 44818 -j DNAT --to-destination $plc_ip:44818 || exit 1
+sudo iptables -A FORWARD -p tcp -d $plc_ip --dport 44818 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT || exit 1
+sudo iptables -A FORWARD -p udp -d $plc_ip --dport 44818 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT || exit 1
 
 # Save iptables rules
-sudo netfilter-persistent save
-
-# Setup Tailscale sub-routing
-echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
-sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
-
-# Activate Tailscale with the advertised routes
-sudo tailscale up --advertise-routes=$plc_ip/32  # Assuming the PLC is a single IP, not a network
-
-# Remove this script
-rm -- "$0"
+sudo netfilter-persistent save || exit 1
