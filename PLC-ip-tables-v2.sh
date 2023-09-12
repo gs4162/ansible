5e6b355daf3efa7e10d3cf181d5120e6ba6a5ad8
gs4162 <81384757+gs4162@users.noreply.github.com>
2023-09-12 12:01:06 +1200

    Update PLC-ip-tables-v2.sh

--- PLC-ip-tables-v2.sh
+++ PLC-ip-tables-v2.sh
@@ -1,79 +1,20 @@
 #!/bin/bash
 
-# Debug: Show current directory and files
-echo "Debug: Current directory and files"
-ls -al
+# Get the path to this script
+SCRIPT_PATH="$0"
 
-# Install iptables-persistent if not installed
-sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y iptables-persistent || exit 1
+# Ask the user for the PLC IP and Tailscale IP, with default values provided
+read -p "Please enter the PLC IP address [default: 192.168.10.102]: " plc_ip
+plc_ip=${plc_ip:-192.168.10.102} # default to 192.168.10.102 if no input
 
-# Ask user for the PLC IP address
-read -p "Please enter the PLC IP address: " plc_ip
-echo "Debug: PLC IP = $plc_ip"
+read -p "Please enter the Tailscale IP address [default: 100.82.83.133]: " tailscale_ip
+tailscale_ip=${tailscale_ip:-100.82.83.133} # default to 100.82.83.133 if no input
 
-# Ask user for the Tailscale IP address (if it's required)
-read -p "Please enter the Tailscale IP address: " tailscale_ip
-echo "Debug: Tailscale IP = $tailscale_ip"
+# Apply the iptables rules using the provided IPs
+sudo iptables -t nat -A PREROUTING -p tcp -d "$tailscale_ip" --dport 44818 -j DNAT --to-destination "$plc_ip":44818
+sudo iptables -t nat -A PREROUTING -p udp -d "$tailscale_ip" --dport 44818 -j DNAT --to-destination "$plc_ip":44818
+sudo iptables -A FORWARD -p tcp -d "$plc_ip" --dport 44818 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A FORWARD -p udp -d "$plc_ip" --dport 44818 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
-# Remove previous rules
-sudo iptables -t nat -D PREROUTING -p tcp --dport 44818 -j DNAT --to-destination $plc_ip:44818 2>/dev/null
-sudo iptables -t nat -D PREROUTING -p udp --dport 44818 -j DNAT --to-destination $plc_ip:44818 2>/dev/null
-sudo iptables -D FORWARD -p tcp -d $plc_ip --dport 44818 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT 2>/dev/null
-sudo iptables -D FORWARD -p udp -d $plc_ip --dport 44818 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT 2>/dev/null
-
-# Reset iptables to default
-sudo iptables -F
-sudo iptables -t nat -F
-sudo iptables -t mangle -F
-sudo iptables -X
-sudo iptables -t nat -X
-sudo iptables -t mangle -X
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-
-# Save iptables rules
-if sudo netfilter-persistent save; then
-    echo "Rules saved successfully."
-else
-    echo "Error saving rules."
-    exit 1
-fi
-
-# Add new iptables rules
-if sudo iptables -t nat -A PREROUTING -p tcp -d $tailscale_ip --dport 44818 -j DNAT --to-destination $plc_ip:44818; then
-    echo "TCP DNAT rule added successfully."
-else
-    echo "Error adding TCP DNAT rule."
-    exit 1
-fi
-
-if sudo iptables -t nat -A PREROUTING -p udp -d $tailscale_ip --dport 44818 -j DNAT --to-destination $plc_ip:44818; then
-    echo "UDP DNAT rule added successfully."
-else
-    echo "Error adding UDP DNAT rule."
-    exit 1
-fi
-
-if sudo iptables -A FORWARD -p tcp -d $plc_ip --dport 44818 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT; then
-    echo "TCP FORWARD rule added successfully."
-else
-    echo "Error adding TCP FORWARD rule."
-    exit 1
-fi
-
-if sudo iptables -A FORWARD -p udp -d $plc_ip --dport 44818 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT; then
-    echo "UDP FORWARD rule added successfully."
-else
-    echo "Error adding UDP FORWARD rule."
-    exit 1
-fi
-
-# Save iptables rules
-if sudo netfilter-persistent save; then
-    echo "Final rules saved successfully."
-else
-    echo "Error saving final rules."
-    exit 1
-fi
+# Delete the script itself
+rm -f "$SCRIPT_PATH"
