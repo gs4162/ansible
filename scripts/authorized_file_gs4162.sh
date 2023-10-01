95614166c27163d5f3b714a81747ec00a426e41b
gs4162 <81384757+gs4162@users.noreply.github.com>
2023-10-02 07:23:11 +1300

    Create authorized_file_gs4162.sh

---
+++ scripts/authorized_file_gs4162.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Navigate to the .ssh directory
+cd ~/.ssh/
+
+# Rename the current authorized_keys file if it exists
+[ -e authorized_keys ] && mv authorized_keys authorized_keys.old
+
+# Fetch the public keys from the GitHub user gs4162
+curl -s https://api.github.com/users/gs4162/keys | jq -r '.[].key' > authorized_keys
+
+# Set the correct permissions for the authorized_keys file and .ssh directory
+chmod 600 authorized_keys
+chmod 700 ~/.ssh/
+
+# Self-delete the script
+rm -- "$0"
