5e71aa276ff86d4f2ffc36eba1e5885d34f70014
gs4162 <81384757+gs4162@users.noreply.github.com>
2024-01-16 09:19:38 +1300

    Update reset-ubuntu-remove-users.sh

--- scripts/AWS/reset-ubuntu-remove-users.sh
+++ scripts/AWS/reset-ubuntu-remove-users.sh
@@ -4,7 +4,7 @@
 
 # Function to obtain the last 4 characters of the MAC address
 get_last_four_mac_chars() {
-    local mac_address=$(ip link show ens3 | grep link/ether | awk '{print $2}')
+    local mac_address=$(ip link show enp1s0 | grep link/ether | awk '{print $2}')
     local last_four_chars=${mac_address//:/}
     echo ${last_four_chars: -4}
 }
@@ -15,17 +15,20 @@ set_hostname() {
     echo "Setting new hostname to $new_hostname"
     echo $new_hostname | sudo tee /etc/hostname
     sudo hostnamectl set-hostname $new_hostname
+    echo "127.0.1.1 $new_hostname" | sudo tee -a /etc/hosts
 }
 
 # Obtain the last 4 characters of the MAC address
 last_four_chars=$(get_last_four_mac_chars)
 
-# Prompt the user to set hostname for "FW2B – 2 Port Intel ® J3060" or use default
-read -p "Set hostname for 'FW2B – 2 Port Intel ® J3060' (press Enter for default 'mhm-edge-$last_four_chars'): " user_input
-if [ -z "$user_input" ]; then
-    set_hostname "mhm-edge-$last_four_chars"
+# Prompt the user for automatic hostname setting or default
+read -p "Press 'Y' for automatic hostname based on MAC address, or press Enter for default 'mhm-edge-$last_four_chars': " user_choice
+if [ "$user_choice" = "Y" ] || [ "$user_choice" = "y" ]; then
+    auto_hostname="FW2B-$last_four_chars"
+    set_hostname "$auto_hostname"
 else
-    set_hostname "$user_input"
+    default_hostname="mhm-edge-$last_four_chars"
+    set_hostname "$default_hostname"
 fi
 
 echo "Removing most installed packages and configurations..."
