299a7d6298c0e709ac1c0fd8419991421bc963a0
gs4162 <81384757+gs4162@users.noreply.github.com>
2024-01-16 09:09:32 +1300

    Update reset-ubuntu-remove-users.sh

--- scripts/AWS/reset-ubuntu-remove-users.sh
+++ scripts/AWS/reset-ubuntu-remove-users.sh
@@ -2,14 +2,37 @@
 
 # Warning: This script will make significant changes to your system.
 
+# Function to obtain the last 4 characters of the MAC address
+get_last_four_mac_chars() {
+    local mac_address=$(ip link show ens3 | grep link/ether | awk '{print $2}')
+    local last_four_chars=${mac_address//:/}
+    echo ${last_four_chars: -4}
+}
+
+# Function to set hostname
+set_hostname() {
+    local new_hostname=$1
+    echo "Setting new hostname to $new_hostname"
+    echo $new_hostname | sudo tee /etc/hostname
+    sudo hostnamectl set-hostname $new_hostname
+}
+
+# Obtain the last 4 characters of the MAC address
+last_four_chars=$(get_last_four_mac_chars)
+
+# Prompt the user to set hostname for "FW2B – 2 Port Intel ® J3060" or use default
+read -p "Set hostname for 'FW2B – 2 Port Intel ® J3060' (press Enter for default 'mhm-edge-$last_four_chars'): " user_input
+if [ -z "$user_input" ]; then
+    set_hostname "mhm-edge-$last_four_chars"
+else
+    set_hostname "$user_input"
+fi
+
 echo "Removing most installed packages and configurations..."
 sudo apt-get remove --purge -y $(dpkg --get-selections | grep -v deinstall | awk '{print $1}')
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
 sudo rm -rf /home/* /root/*
-
-echo "Resetting hostname..."
-sudo hostnamectl set-hostname ubuntu
 
 echo "Disabling SSH service..."
 sudo systemctl disable ssh
